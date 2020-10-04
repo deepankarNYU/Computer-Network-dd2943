10caa43017197652537720a8d87b461f036c5fe8
deepankarNYU <60680536+deepankarNYU@users.noreply.github.com>
2020-10-03 22:42:52 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,13 @@
 #import socket module
 from socket import *
 import sys # In order to terminate the program
-
 def webServer(port=13331):
     serverSocket = socket(AF_INET, SOCK_STREAM)
-
     #Prepare a sever socket
     #Fill in start
     serverSocket.bind(("", port))
     serverSocket.listen(1)
     #Fill in end
-
     while True:
         #Establish the connection
         #print('Ready to serve...')
@@ -20,16 +17,13 @@ def webServer(port=13331):
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()#Fill in start #Fill in end
-
             #Send one HTTP header line into socket
             #Fill in start
             connectionSocket.send("200 OK\r\n".encode())
             #Fill in end
-
             #Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
-
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
@@ -37,14 +31,11 @@ def webServer(port=13331):
             #Fill in start
             connectionSocket.send("404 Not Found\n\r".encode())
             #Fill in end
-
             #Close client socket
             #Fill in start
             connectionSocket.close()
             #Fill in end
-
     serverSocket.close()
     sys.exit()  # Terminate the program after sending the corresponding data
-
 if __name__ == "__main__":
     webServer(13331)
